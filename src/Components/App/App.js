37dd80f925f999a14adbfb392e420ab03747967c
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-22 19:51:19 -0700

    Add redirect path for user auth

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from '../Landing Page/LandingPage';
+import LoadingPage from '../Loading Page/LoadingPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/redirect' element={<LoadingPage />} />
       </Routes>
     </Router>
   );
