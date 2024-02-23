c4a6e231bc4b3219721130972acca1cec509a053
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-23 13:43:45 -0700

    Add getAthleteActivities and addAthleteToAPI

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -3,7 +3,7 @@ export const redirectToStravaAuthorization = () => {
   const redirectUri = 'http://localhost:3000/redirect/';
   const responseType = 'code';
   const approvalPrompt = 'auto';
-  const scope = 'activity:write,read';
+  const scope = 'activity:write,read,activity:read_all';
 
   const authorizationUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&approval_prompt=${approvalPrompt}&scope=${scope}`;
 
@@ -46,11 +46,58 @@ export const handleAuthorizationCallback = async () => {
 export const getAthleteData = async () => {
   const accessToken = localStorage.getItem('stravaAccessToken');
   const refreshToken = localStorage.getItem('stravaRefreshToken');
-  
+
   const response = await fetch('https://www.strava.com/api/v3/athlete', {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
-  })
-    .then((response) => response.json())
+  });
+
+  if (!response.ok) {
+    console.log('Request failed');
+    return;
+  }
+
+  const data = await response.json();
+  addAthleteToAPI(data);
+
+  return data;
+};
+
+export const getAthleteActivities = async () => {
+  const accessToken = localStorage.getItem('stravaAccessToken');
+  const refreshToken = localStorage.getItem('stravaRefreshToken');
+  let page = 1;
+  let workouts = [];
+
+  while (true) {
+    const requests = await fetch(
+      `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}&per_page=200&page=${page}`
+    );
+    const data = await requests.json();
+
+    if (!data.length) break;
+
+    workouts = workouts.concat(data);
+    page++;
+  }
+
+  localStorage.removeItem('stravaAccessToken');
+  localStorage.removeItem('stravaRefreshToken');
+
+  return workouts;
+};
+
+export const addAthleteToAPI = async (athlete) => {
+  const response = await fetch('http://localhost:3001/api/v1/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(athlete),
+  });
+
+  const data = await response.json();
+
+  return data;
 };
