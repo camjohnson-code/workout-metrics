c10c75641a73310debd5a0115733bba827190a43
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-23 13:37:29 -0700

    Refactor app syntax

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from '../Landing Page/LandingPage';
 import LoadingPage from '../Loading Page/LoadingPage';
+import Dashboard from '../Dashboard/Dashboard';
 
-function App() {
+
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/redirect' element={<LoadingPage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
