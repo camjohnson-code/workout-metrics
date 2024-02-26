a700fd91a2eb7e355550abc6b39f5852dd913082
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-25 17:41:55 -0700

    Add heatmap component

--- src/Components/Heatmap/Heatmap.js
+++ src/Components/Heatmap/Heatmap.js
@@ -1,11 +1,103 @@
 import './Heatmap.css';
 import Sidebar from '../Sidebar/Sidebar';
+import { useEffect, useState } from 'react';
+import DeckGL from '@deck.gl/react';
+import { LineLayer } from '@deck.gl/layers';
+import { Map } from 'react-map-gl';
+import GL from '@luma.gl/constants';
+import polyline from '@mapbox/polyline';
+import Lottie from "lottie-react";
+import LoadingAnimation from '../../Animations/loading.json'
 
-const Heatmap = ({ year, athlete }) => {
+const Heatmap = ({ year, athlete, homeCoordinates, activities }) => {
+  const [loading, setLoading] = useState(true);
+  const [polylines, setPolylines] = useState([]);
+  const [lineCoordinates, setLineCoordinates] = useState([]);
+
+  useEffect(() => {
+    if (activities.length) {
+      const polylines = activities.map(
+        (activity) => activity.map.summary_polyline
+      );
+      setPolylines(polylines);
+    }
+  }, []);
+
+  useEffect(() => {
+    const encryptedPolylines = polylines;
+    const decryptedPolylines = encryptedPolylines.map((line) =>
+      polyline.decode(line)
+    );
+    const flippedLngLat = decryptedPolylines.map((array) =>
+      array.map(([lat, lng]) => [lng, lat])
+    );
+
+    const lineCoordinates = formatCoordinatesPairs(flippedLngLat).filter(
+      (obj) => obj
+    );
+
+    setLineCoordinates(lineCoordinates);
+    setLoading(false);
+  }, [polylines]);
+
+  const formatCoordinatesPairs = (array) => {
+    const nestedCoordinatePairs = array.map((coordinatesArray, index) =>
+      coordinatesArray.map((coordinates, index) => {
+        if (coordinatesArray[index + 1]) {
+          return {
+            sourcePosition: coordinates,
+            targetPosition: coordinatesArray[index + 1],
+          };
+        }
+      })
+    );
+
+    return [].concat(...nestedCoordinatePairs);
+  };
+
+  const INITIAL_VIEW_STATE = {
+    longitude: homeCoordinates[0],
+    latitude: homeCoordinates[1],
+    zoom: 10,
+    pitch: 0,
+    bearing: 0,
+  };
+
+  const layers = [
+    new LineLayer({
+      id: 'line-layer',
+      data: lineCoordinates,
+      getColor: () => [252, 160, 99],
+      opacity: 0.05,
+    }),
+  ];
+
   return (
-    <section className='heatmap'>
-        <Sidebar athlete={athlete} year={year}></Sidebar>
-      <h1>Heatmap</h1>
+    <section className='heatmap-page'>
+      <Sidebar athlete={athlete} year={year}></Sidebar>
+      <section className='heatmap-container'>
+        {loading && <Lottie animationData={LoadingAnimation} style={{ width: 300, height: 300 }} />}
+        {activities.length ? (
+          <DeckGL
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+            layers={layers}
+            parameters={{
+              blend: true,
+              blendFunc: [GL.SRC_ALPHA, GL.ONE, GL.ONE_MINUS_DST_ALPHA, GL.ONE],
+              blendEquation: GL.FUNC_ADD,
+            }}
+          >
+            <Map
+              mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+              mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
+              attributionControl={false}
+            />
+          </DeckGL>
+        ) : (
+          <p className='no-gps'>None of your activities have GPS data!</p>
+        )}
+      </section>
     </section>
   );
 };
