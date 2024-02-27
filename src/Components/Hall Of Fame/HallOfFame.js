0871d67c6e45e5aa9db167f54cb5abf232831e56
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-26 23:21:30 -0700

    Add HallOfFame component

--- src/Components/Hall Of Fame/HallOfFame.js
+++ src/Components/Hall Of Fame/HallOfFame.js
@@ -1,11 +1,166 @@
 import './HallOfFame.css';
 import Sidebar from '../Sidebar/Sidebar';
+import { useState, useEffect } from 'react';
+import { fetchUserActivities } from '../../ApiCalls';
+import Card from '../Card/Card';
+import {
+  addFavoriteToHallOfFame,
+  removeFavoriteFromHallOfFame,
+  getHallOfFameActivities,
+} from '../../ApiCalls';
 
-const HallOfFame = ({ year, athlete }) => {
+const HallOfFame = ({
+  year,
+  athlete,
+  convertMtoMiles,
+  convertSecondsToHMS,
+  formatDate,
+}) => {
+  const [favorites, setFavorites] = useState([]);
+  const [activities, setActivities] = useState([]);
+  const [keywords, setKeywords] = useState('');
+  const [activityType, setActivityType] = useState('all');
+  const [hasSearched, setHasSearched] = useState(false);
+  const isValidForm = !keywords;
+
+  useEffect(() => {
+    getHallOfFameActivities(athlete).then((favorites) =>
+      setFavorites(favorites)
+    );
+  }, [athlete]);
+
+  useEffect(() => {
+    if (!keywords) setActivities([]);
+  }, [keywords]);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setHasSearched(true);
+    const fetchData = async () => {
+      const activities = await fetchUserActivities(
+        athlete,
+        keywords,
+        activityType
+      );
+      setActivities(activities);
+    };
+
+    fetchData();
+  };
+
+  const handleStarClick = async (activity) => {
+    const isFavorite = favorites.some(
+      (favorite) => favorite.id === activity.id
+    );
+    if (isFavorite) {
+      setFavorites(favorites.filter((favorite) => favorite.id !== activity.id));
+      try {
+        await removeFavoriteFromHallOfFame(activity.id);
+      } catch (error) {
+        console.error('Error removing favorite from Hall of Fame:', error);
+      }
+    } else {
+      setFavorites([...favorites, activity]);
+      try {
+        await addFavoriteToHallOfFame(activity);
+      } catch (error) {
+        console.error('Error adding favorite to Hall of Fame:', error);
+      }
+    }
+  };
+
+  const cards = activities.map((activity, index) => {
+    const isFavorite = favorites.some(
+      (favorite) => favorite.id === activity.id
+    );
+
+    return (
+      <Card
+        formatDate={formatDate}
+        key={index}
+        convertSecondsToHMS={convertSecondsToHMS}
+        convertMtoMiles={convertMtoMiles}
+        activity={activity}
+        key={activity.id}
+        onStarClick={() => handleStarClick(activity)}
+        isFavorite={isFavorite}
+      />
+    );
+  });
+
+  const favoriteCards = favorites.map((activity, index) => {
+    return (
+      <Card
+        formatDate={formatDate}
+        key={index}
+        convertSecondsToHMS={convertSecondsToHMS}
+        convertMtoMiles={convertMtoMiles}
+        activity={activity}
+        key={activity.id}
+        onStarClick={() => handleStarClick(activity)}
+        isFavorite={true}
+      />
+    );
+  });
+
   return (
     <section className='hall-of-fame'>
-        <Sidebar athlete={athlete} year={year}></Sidebar>
-      <h1>Hall of Fame</h1>
+      <Sidebar athlete={athlete} year={year}></Sidebar>
+      <section className='hall-of-fame-section'>
+        <h1 className='hall-of-fame-title'>
+          {athlete.firstname}'s Hall of Fame
+        </h1>
+        <form className='hall-of-fame-form' onSubmit={handleFormSubmit}>
+          <section className='selector'>
+            <label htmlFor='keywords'>Keywords:</label>
+            <input
+              type='text'
+              id='keywords'
+              name='keywords'
+              placeholder='Search for activities'
+              value={keywords}
+              onChange={(e) => setKeywords(e.target.value)}
+            />
+          </section>
+
+          <section className='selector'>
+            <label htmlFor='activity-type'>Activity Type:</label>
+            <select
+              onChange={(e) => setActivityType(e.target.value)}
+              value={activityType}
+              name='activity-type'
+              id='activity-type'
+            >
+              <option value='all'>All</option>
+              <option value='run'>Run</option>
+              <option value='ride'>Ride</option>
+              <option value='swim'>Swim</option>
+            </select>
+          </section>
+          <button
+            disabled={isValidForm}
+            type='submit'
+            className='submit-button'
+          >
+            Search
+          </button>
+        </form>
+        {!!favorites.length && !keywords && (
+          <section className='card-container'>{favoriteCards}</section>
+        )}
+        {!favorites.length && !keywords && (
+          <p className='no-saved-activities'>
+            Save memorable activities using the form above.
+          </p>
+        )}
+        {keywords && !hasSearched && <p>Press submit to see your results.</p>}
+        {activities.length > 0 && keywords && (
+          <section className='card-container'>{cards}</section>
+        )}
+        {!activities.length && keywords && hasSearched && (
+          <p>No activities found. Try your search again.</p>
+        )}
+      </section>
     </section>
   );
 };
