66e9e831fe68017f3cdfddea2d3c4091ecaa1b42
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-27 18:56:04 -0700

    Add forms to Add Workout component

--- src/Components/Add Workout/AddWorkout.js
+++ src/Components/Add Workout/AddWorkout.js
@@ -1,11 +1,57 @@
 import './AddWorkout.css';
 import Sidebar from '../Sidebar/Sidebar';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import ManualForm from '../Manual Form/ManualForm';
+import FileUploader from '../File Uploader/FileUploader';
 
 const AddWorkout = ({ year, athlete }) => {
+  const [manualForm, setManualForm] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+
+  // useEffect(() => {
+  //   handleSubmit();
+  // }, [submitted]);
+
   return (
     <section className='add-workout'>
-        <Sidebar athlete={athlete} year={year}></Sidebar>
-      <h1>Add Workout</h1>
+      <Sidebar athlete={athlete} year={year}></Sidebar>
+      <section className='upload-section'>
+        {!submitted && <h1 className='upload-title'>Missing an activity?</h1>}
+        {!submitted && (
+          <p className='upload-subtitle'>
+            Add your activity and we’ll add it to your Strava profile.
+          </p>
+        )}
+        {!submitted &&
+          (!manualForm ? (
+            <FileUploader
+              setSubmitted={setSubmitted}
+              setManualForm={setManualForm}
+              athlete={athlete}
+            />
+          ) : (
+            <ManualForm
+              setSubmitted={setSubmitted}
+              manualForm={manualForm}
+              setManualForm={setManualForm}
+              athlete={athlete}
+            />
+          ))}
+        {submitted && (
+          <p className='submitted-message'>
+            Submitted! Return to the{' '}
+            <Link className='manual-link' to='/dashboard'>
+              home page
+            </Link>
+            .
+          </p>
+        )}
+      </section>
     </section>
   );
 };
