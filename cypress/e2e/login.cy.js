fac5a91039ce9e2a3ae04dd5dab1eef1bebfbd3c
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-03-02 13:41:38 -0700

    Add test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,4 @@
-describe('Random Cocktail Generator', () => {
+describe('Log in and see the dashboard', () => {
   beforeEach(() => {
     cy.intercept(
       'https://api.mapbox.com/geocoding/v5/mapbox.places/%2C%20.json?access_token=pk.eyJ1IjoiY2Ftam9obnNvbi1jb2RlIiwiYSI6ImNsczVoZmQ5cTFoNzEyaWxkMHExZGxocnkifQ.HbZsLy0fXhbkSKTmVAIS0w',
@@ -6,7 +6,8 @@ describe('Random Cocktail Generator', () => {
         response: 200,
         fixture: 'quote',
       }
-    );
+    ).as('mapbox');
+
     cy.intercept(
       'https://api.weatherapi.com/v1/forecast.json?key=3b20c29161a847a880110041242502&q=0,0&days=1&aqi=no&alerts=no',
       {
@@ -14,87 +15,74 @@ describe('Random Cocktail Generator', () => {
         fixture: 'weather',
       }
     ).as('weather');
+    
+    cy.intercept(
+      'https://api.mapbox.com/geocoding/v5/mapbox.places/Highlands%20Ranch%2C%20Colorado.json?access_token=pk.eyJ1IjoiY2Ftam9obnNvbi1jb2RlIiwiYSI6ImNsczVoZmQ5cTFoNzEyaWxkMHExZGxocnkifQ.HbZsLy0fXhbkSKTmVAIS0w',
+      {
+        response: 200,
+        fixture: 'userWeather',
+      }
+    ).as('userWeather');
+
     cy.intercept('http://localhost:3001/api/v1/quote', {
       response: 200,
       fixture: 'quote',
     }).as('quote');
 
+    cy.intercept('https://c.strava.com/com.snowplowanalytics.snowplow/tp2', {
+      response: 200,
+      fixture: 'stravaLogin',
+    }).as('stravaLogin');
+
+    cy.intercept('https://www.strava.com/api/v3/athlete', {
+      response: 200,
+      fixture: 'athlete',
+    }).as('athleteResponse');
+
+    cy.intercept('http://localhost:3001/api/v1/users', {
+      response: 200,
+      fixture: 'athlete',
+    }).as('athletePost');
+
+    cy.intercept('http://localhost:3001/api/v1/users/132610471', {
+      response: 200,
+      fixture: 'athlete',
+    }).as('athlete');
+
+    cy.intercept('https://www.strava.com/api/v3/athlete/activities?access_token=null&per_page=200&page=1', {
+      response: 200,
+      fixture: 'activities',
+    }).as('activities');
+
+    cy.intercept('https://www.strava.com/api/v3/athlete/activities?access_token=null&per_page=200&page=2', {
+      response: 200,
+      fixture: 'noActivities',
+    }).as('noActivities');
+
+    cy.intercept('https://api.weatherapi.com/v1/forecast.json?key=3b20c29161a847a880110041242502&q=39.549724,-104.969566&days=1&aqi=no&alerts=no', {
+      response: 200,
+      fixture: 'weather',
+    }).as('userWeather');
+
+    cy.intercept('http://localhost:3001/api/v1/activities', {
+      response: 200,
+      fixture: 'activities',
+    }).as('activities');
+
     cy.visit('http://localhost:3000/');
   });
 
-  it('Visits the landing page', () => {
-    cy.get('.title').contains('WorkoutMetrics.fit');
-    cy.get('.subtitle').contains(
-      'Empower your training with insightful metrics'
-    );
-    cy.get('.strava-connect-btn');
-    cy.get('.down-icon').click();
-    cy.get('.features-images').children().should('have.length', 3);
-
-    cy.get('.features-text')
-      .find('h2.feature-heading')
-      .contains('Unique tools for all your data visualization');
-
-    cy.get('.features-text')
-      .find('p.feature-subtitle')
-      .contains(
-        'Use our tools to track your data at a glance. Then share your achievements easily'
-      );
-
-    cy.get('.features-text')
-      .find('ul')
-      .children()
-      .should('have.length', 3)
-      .then(($li) => {
-        cy.wrap($li[0]).find('svg').should('exist');
-        cy.wrap($li[0])
-          .find('p')
-          .contains('Heatmaps - See where you’ve traveled');
-
-        cy.wrap($li[1]).find('svg').should('exist');
-        cy.wrap($li[1]).find('p').contains('Charts - Your stats, visualized');
-
-        cy.wrap($li[2]).find('svg').should('exist');
-        cy.wrap($li[2])
-          .find('p')
-          .contains('Easy share - Showcase your achievements easily');
-      });
-
-    cy.get('.faq').find('h3').contains('Frequently Asked Questions');
-    cy.get('section.questions-section')
-      .find('div.szh-accordion')
-      .children()
-      .should('have.length', 5)
-      .each(($child) => {
-        cy.wrap($child).should('have.class', '');
-
-        const itemDiv = $child.find('div.szh-accordion__item');
-        cy.wrap(itemDiv).should('exist');
-
-        const headingH3 = itemDiv.find('h3.szh-accordion__item-heading');
-        cy.wrap(headingH3).should('exist');
-
-        const button = headingH3.find('button.szh-accordion__item-btn');
-        cy.wrap(button).should('exist');
-
-        const contentDiv = itemDiv.find('div.szh-accordion__item-content');
-        cy.wrap(contentDiv).should('exist');
-      });
-
-    cy.get('.footer p').should('contain', '© Cameron Johnson, 2024.');
-    cy.get('.footer img.strava-power-btn').should('exist');
-
-    cy.get('header.header.visible')
-      .find('a.header-link')
-      .should('have.length', 3)
-      .then(($links) => {
-        cy.wrap($links.eq(2)).click();
-        cy.wrap($links.eq(1)).click();
-        cy.wrap($links.eq(0)).click();
-      });
-
-    cy.get('header')
-      .should('have.class', 'header')
-      .and('not.have.class', 'visible');
+  it('Navigates to the dashboard', () => {
+    cy.get('.button-holder').click().visit('http://localhost:3000/redirect');
+    cy.get('.dashboard').children().should('have.length', 8);
+    cy.get('.cell-1').find('h1').should('contain', 'Date');
+    cy.get('.cell-2').find('h1').should('contain', 'Last Activity');
+    cy.get('.cell-3').find('h1').should('contain', 'Achievements YTD'); 
+    cy.get('.cell-4').find('h1').should('contain', "Today's high");
+    cy.get('.cell-5').find('h1').should('contain', 'Relative Effort');
+    cy.get('.cell-6').find('h1').should('contain', 'Longest Activity in 2024');
+    cy.get('.cell-7').find('h1').should('contain', 'Activity Streak');
+    cy.get('.cell-8').find('h1').should('contain', 'Your Daily Kick in the Butt');
+    cy.get('.quote').should('contain', 'Health is the vital principle of bliss, and exercise, of health.');
   });
 });
