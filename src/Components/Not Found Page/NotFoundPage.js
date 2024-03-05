30419edb2c30c2d9e3804a7caa3fcfb84bc01978
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-03-05 10:01:34 -0700

    Refactor error message on NotFoundPage

--- src/Components/Not Found Page/NotFoundPage.js
+++ src/Components/Not Found Page/NotFoundPage.js
@@ -1,13 +1,19 @@
-import './NotFoundPage.css'
+import './NotFoundPage.css';
 import { Link } from 'react-router-dom';
 
-const NotFoundPage = () => {
+const NotFoundPage = ({ isLoggedIn }) => {
   return (
     <section className='not-found-page'>
       <h1 className='not-found-title'>Page not found!</h1>
-      <p className='not-found-subtitle'>Click <Link className='not-found-link' to='/dashboard' >here</Link> to return to the dashboard.</p>
+      <p className='not-found-subtitle'>
+        Click{' '}
+        <Link className='not-found-link' to={isLoggedIn ? '/dashboard' : '/'}>
+          here
+        </Link>{' '}
+        to return {isLoggedIn ? 'to the dashboard' : 'home'}.
+      </p>
     </section>
   );
-}
+};
 
 export default NotFoundPage;
